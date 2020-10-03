a30cf426a33121e3221f840caa607edd8d210587
Zailer43 <minecraftpelusa@gmail.com>
2020-10-03 10:52:27 -0300

    Añade fz!admin
    * Con fz!admin puedes darle permisos de administrador del bot a alguien (menos para fz!di), luego de poner el comando se genera un token en la consola que se debe de  poner en fz!admin <token>

--- fzmm/admin.js
+++ fzmm/admin.js
@@ -20,25 +20,43 @@ function uuid() {
   return uuid;
 }
 
-  function inject(bot, lang, admin, prefix, saltar, seguir) {
+function inject(bot, lang, admin, prefix, saltar, seguir) {
 
   const mcData = require('minecraft-data')(bot.version);
 
   let saltoestado = saltar;
   let seguirestado = seguir;
-  let target;
+  let target, token;
+  let admintemporal = [];
+  admintemporal.push(admin)
   bot.loadPlugin(pathfinder);
   bot.on('chat2', function (username, message) {
-    if (username != admin) return;
     if (message.startsWith(prefix)) {
       message = message.slice(prefix.length)
+      if (message === 'admin') {
+        token = uuid();
+        console.log(lang.admin.eltoken + token);
+        bot.chat(lang.admin.introducetoken)
+      }  else if (message.toLowerCase().startsWith('admin ')) {
+        const admincmd = message.split(' ');
+        if (!token) {
+          bot.chat(lang.admin.nohaytoken+ prefix + 'admin');
+        } else if (admincmd[1] === token) {
+          admintemporal.push(username);
+          bot.chat(lang.admin.eresadmin);
+          token = null;
+        } else {
+          bot.chat(lang.admin.tokeninvalido);
+        }
+      }
+      if (!admintemporal.includes(username)) return;
       switch (message) {
         case 'tp':
-          bot.chat('/tp ' + admin);
-          console.log(lang.tepeado + admin);
+          bot.chat('/tp ' + username);
+          console.log(lang.tepeado + username);
           break;
         case 'gmc':
-          bot.chat( lang.gamemode + lang.gmc);
+          bot.chat(lang.gamemode + lang.gmc);
           console.log(lang.gamemode + lang.gmc);
           break;
         case 'gms':
@@ -60,14 +78,14 @@ function uuid() {
         case 'nukereal':
           bot.chat('/playsound entity.generic.explode master @a ~ ~ ~');
           console.log(lang.nukereal);
+          break;
       }
     }
-  }
-  )
+  })
   bot.on('guardarcoord', function (username, x, y, z, lugar) {
     if (username != admin) return;
     const path = require('path');
-    const coordsdirectorio = path.join(__dirname,'datos/coords.json');
+    const coordsdirectorio = path.join(__dirname, 'datos/coords.json');
     const json_coords = fs.readFileSync(coordsdirectorio, 'utf-8');
 
     let coordenada = JSON.parse(json_coords);
@@ -84,20 +102,20 @@ function uuid() {
       y,
       z
     };
-    
+
     coordenada.push(newCoord);
     fs.writeFileSync(coordsdirectorio, JSON.stringify(coordenada, null, 2), 'utf-8');
 
     bot.chat(lang.guardarcoords)
     console.log(lang.guardarcoords);
   })
-  
+
   navigatePlugin(bot);
 
   bot.on('whisper', function (username, message, rawMessage) {
     if (username === bot.username) return;
     console.log(username + lang.tell + message);
-    if (username != admin) return;
+    if (!admintemporal.includes(username)) return;
     if (message.startsWith(prefix)) {
       message = message.slice(prefix.length)
       switch (message) {
@@ -109,18 +127,18 @@ function uuid() {
           bot.chat('/tpaccept');
           break;
         case 'ven':
-          target = bot.players[admin] ? bot.players[admin].entity : null;
+          target = bot.players[username] ? bot.players[username].entity : null;
           if (!target) return;
           bot.navigate.to(target.position);
           break;
         case 'seguir':
-          target = bot.players[admin].entity;
+          target = bot.players[username].entity;
           seguirestado = !seguirestado
           if (seguirestado) {
             const defaultmove = new Movements(bot, mcData);
             bot.pathfinder.setMovements(defaultmove);
             bot.pathfinder.setGoal(new GoalFollow(target, 2), true);
-          } else  if (!seguirestado) {
+          } else if (!seguirestado) {
             bot.pathfinder.setGoal(null)
           }
           break;
@@ -128,23 +146,24 @@ function uuid() {
           console.log(bot.entity.position.toString());
           break;
       }
+
       function tossNext() {
         if (bot.inventory.items().length === 0) return
         const item = bot.inventory.items()[0]
         bot.tossStack(item, tossNext)
       }
       if (message.startsWith('di ')) {
+        if (username != admin) return;
         bot.chat(message.slice(3));
         console.log('Dije: ' + message.slice(3));
 
       } else if (message.startsWith('hat ')) {
         bot.creative.setInventorySlot(5, new Item(message.slice(4), 1));
-        
+
       } else if (message.startsWith('drop ')) {
         bot.creative.setInventorySlot(36, new Item(message.slice(5), 64));
         tossNext();
       }
     }
-  }
-  )
+  })
 }
